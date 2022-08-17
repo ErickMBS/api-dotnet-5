e79490162a21af63eb090a942d6679e1e9abe576
erick_braga <@Erickmatheus07>
2022-08-17 06:46:39 -0300

    Criar rota para redefinir a senha

--- UsuariosApi/Controllers/LoginController.cs
+++ UsuariosApi/Controllers/LoginController.cs
@@ -31,5 +31,13 @@ namespace UsuariosApi.Controllers
             if (resultado.IsFailed) return Unauthorized(resultado.Errors);
             return Ok(resultado.Successes);
         }
+        
+        [HttpPost("/efetua-reset")]
+        public IActionResult ResetaSenhaUsuario(EfetuaResetRequest request)
+        {
+            Result resultado = _loginService.ResetaSenhaUsuario(request);
+            if (resultado.IsFailed) return Unauthorized(resultado.Errors);
+            return Ok(resultado.Successes);
+        }
     }
 }
--- UsuariosApi/Services/LoginService.cs
+++ UsuariosApi/Services/LoginService.cs
@@ -22,36 +22,53 @@ namespace UsuariosApi.Services
         {
             var resultadoIdentity = _signInManager
                 .PasswordSignInAsync(request.Username, request.Password, false, false);
-            if (resultadoIdentity.Result.Succeeded)
-            {
-                var identityUser = _signInManager
-                    .UserManager
-                    .Users
-                    .FirstOrDefault(usuario => 
-                        usuario.NormalizedUserName == request.Username.ToUpper());
-                Token token = _tokenService.CreateToken(identityUser);
-                return Result.Ok().WithSuccess(token.Value);
-            }
-            return Result.Fail("Login falhou");
+            if (!resultadoIdentity.Result.Succeeded) 
+                return Result.Fail("Login falhou");
+            
+            var identityUser = _signInManager
+                .UserManager
+                .Users
+                .FirstOrDefault(usuario => 
+                    usuario.NormalizedUserName == request.Username.ToUpper());
+            var token = _tokenService.CreateToken(identityUser);
+            return Result.Ok().WithSuccess(token.Value);
         }
 
         public Result SolicitaResetSenhaUsuario(SolicitaResetRequest request)
         {
+            var identityUser = RecuperaUsuarioPorEmail(request.Email);
+            if (identityUser == null) 
+                return Result.Fail("Falha ao solicitar redefinição");
+            
+            var codigoDeRecuperacao = _signInManager
+                .UserManager
+                .GeneratePasswordResetTokenAsync(identityUser)
+                .Result;
+            return Result.Ok().WithSuccess(codigoDeRecuperacao);
+
+        }
+
+        public Result ResetaSenhaUsuario(EfetuaResetRequest request)
+        {
+            var identityUser = RecuperaUsuarioPorEmail(request.Email);
+            
+            var resultadoIdentity = _signInManager
+                .UserManager
+                .ResetPasswordAsync(identityUser, request.Token, request.Password)
+                .Result;
+            
+            return resultadoIdentity.Succeeded 
+                ? Result.Ok().WithSuccess("Senha redefinida com sucesso!") 
+                : Result.Fail("Falha ao redefinir a senha!");
+        }
+
+        private IdentityUser<int> RecuperaUsuarioPorEmail(string email)
+        {
             var identityUser = _signInManager
                 .UserManager
                 .Users
-                .FirstOrDefault(u => u.NormalizedEmail == request.Email.ToUpper());
-
-            if (identityUser != null)
-            {
-                var codigoDeRecuperacao = _signInManager
-                    .UserManager
-                    .GeneratePasswordResetTokenAsync(identityUser)
-                    .Result;
-                return Result.Ok().WithSuccess(codigoDeRecuperacao);
-            }
-
-            return Result.Fail("Falha ao solicitar redefinição");
+                .FirstOrDefault(u => u.NormalizedEmail == email.ToUpper());
+            return identityUser;
         }
     }
 }
